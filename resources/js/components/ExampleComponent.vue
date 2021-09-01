905d615c3b2ddff3715e13d8db1b20b8c7183f4e
aitalvivem <52562995+aitalvivem@users.noreply.github.com>
2021-09-01 14:49:28 +0200

    Add files via upload

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Jeu</div>
+
+                    <div class="card-body">
+                        coucou
+                        <tr v-for="profil in profils" :key="profil.id">
+                            <td>{{ profil.name }}<td>
+                        </tr>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
